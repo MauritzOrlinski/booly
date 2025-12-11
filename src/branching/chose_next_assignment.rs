1e52840d78699ad63682c44f1c5a34127067b7eb
Maximilian Hofstetter <maximilian.hofstetter@gmx.net>
2025-12-11 10:37:50 +0100

    Branching

--- src/branching/chose_next_assignment.rs
+++ src/branching/chose_next_assignment.rs
@@ -1,8 +1,9 @@
-use crate::assignment::assignment::Assignment;
+use crate::assignment::assignments::Assignments;
+use crate::assignment::single_assignment::SingleAssignment;
 use crate::cnf::cnf_formula::CnfFormula;
 
-pub type MultiAssignment = Vec<Assignment>;
+pub type MultiAssignment = Vec<SingleAssignment>;
 
 pub trait Branching {
-    fn chose_branches(cnf_formula: &CnfFormula) -> Vec<MultiAssignment>;
+    fn chose_branches(cnf_formula: &CnfFormula) -> Vec<Assignments>;
 }
--- src/branching/monien_speckenmeyer.rs
+++ src/branching/monien_speckenmeyer.rs
@@ -1,23 +1,31 @@
-use crate::assignment::assignment::{Assignment, AssignmentReason};
+use crate::assignment::assignments::Assignments;
+use crate::assignment::single_assignment::{SingleAssignment, AssignmentReason};
 use crate::branching::chose_next_assignment::{Branching, MultiAssignment};
 use crate::cnf::cnf_formula::CnfFormula;
 
 pub(crate) struct MonienSpeckenmeyer;
 
 impl Branching for MonienSpeckenmeyer {
-    fn chose_branches(cnf_formula: &CnfFormula) -> Vec<MultiAssignment> {
+    fn chose_branches(cnf_formula: &CnfFormula) -> Vec<Assignments> {
         let shortest_clause = cnf_formula
             .clauses
             .iter()
             .map(|(_, clause)| clause)
+            .filter(|clause| matches!(clause.satisfied_by, None))
             .min_by(|a, b| (&a.unassigned_variables).cmp(&b.unassigned_variables))
             .unwrap();
 
-        let mut branches: Vec<MultiAssignment> = vec![];
-        let mut unsat_assignments = MultiAssignment::new();
+        let mut branches: Vec<Assignments> = vec![];
+        let mut unsat_assignments = Assignments::new();
 
-        for (satisfying_variable_id, polarity) in &shortest_clause.literals {
-            let sat_assignment = Assignment::new(
+        for (satisfying_variable_id, polarity) in shortest_clause.literals.iter() {
+
+            let variable = cnf_formula.variables.get(satisfying_variable_id).unwrap();
+            if matches!(variable.value, Some(_)) {
+                continue;
+            }
+
+            let sat_assignment = SingleAssignment::new(
                 *satisfying_variable_id,
                 polarity.get_satisfying_assignment(),
                 AssignmentReason::Branching,
@@ -27,7 +35,7 @@ impl Branching for MonienSpeckenmeyer {
             branch.push(sat_assignment);
             branches.push(branch);
 
-            let unsat_assignment = Assignment::new(
+            let unsat_assignment = SingleAssignment::new(
                 *satisfying_variable_id,
                 polarity.get_satisfying_assignment().get_inverse(),
                 AssignmentReason::Branching,
@@ -35,6 +43,7 @@ impl Branching for MonienSpeckenmeyer {
 
             unsat_assignments.push(unsat_assignment);
         }
+        branches.push(unsat_assignments);
         branches
     }
 }
