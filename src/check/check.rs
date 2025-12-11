2f516148fefc0e62f42944a562ad4e19021d51c8
Maximilian Hofstetter <maximilian.hofstetter@gmx.net>
2025-12-11 10:37:56 +0100

    Check

---
+++ src/check/check.rs
@@ -0,0 +1,12 @@
+use crate::cnf::cnf_formula::CnfFormula;
+
+pub fn check_satisfied(cnf_formula: &CnfFormula) -> bool {
+    cnf_formula.clauses.iter().all(|(_, clause)| {
+        if let Some(satisfying_var_id) = clause.satisfied_by {
+            let satisfying_var = cnf_formula.variables.get(&satisfying_var_id).unwrap();
+            let var_literal_polarity = clause.literals.get_polarity_for_var(&satisfying_var_id).unwrap();
+            return var_literal_polarity.get_satisfying_assignment() == satisfying_var.value.unwrap();
+        }
+        false
+    })
+}
