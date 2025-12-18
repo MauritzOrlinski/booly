c51c60b33be9fe827f401d77b60ba63f3d817147
Maximilian Hofstetter <maximilian.hofstetter@gmx.net>
2025-12-18 18:18:48 +0100

    WIP

--- src/dpll/unit_propagation.rs
+++ src/dpll/unit_propagation.rs
@@ -5,6 +5,13 @@ use crate::dpll::dpll::Dpll;
 use crate::dpll::dpll::DpllResult;
 
 impl Dpll {
+
+    /// Execute unit propagation. As long as there are unit clauses present in the unit queue,
+    /// satisfy them. If this creates any new unit clauses, add them to the queue.
+    ///
+    /// # Returns
+    /// An optional DPLL result. `Satisfied`, if the cnf has been satisfied during unit propagation.
+    /// `Conflict`, if there has been a conflict during unit propagation. `None` otherwise.
     pub(crate) fn propagate_unit_clauses(&mut self) -> Option<DpllResult> {
         while let Some(unit_clause_id) = self.unit_queue.pop_front() {
             let unit_clause = self.cnf_formula.clauses.get(unit_clause_id).unwrap();
@@ -28,6 +35,13 @@ impl Dpll {
         None
     }
 
+    /// Finds the satisfying assignment for a unit clause.
+    ///
+    /// # Arguments
+    /// * `unit_clause` - The unit clause. Expected to have only one unassigned literal left.
+    ///
+    /// # Returns
+    /// The satisfying assignment.
     fn find_satisfying_assignment_for_unit_clause(&self, unit_clause: &Clause) -> Assignment {
         unit_clause
             .literals
