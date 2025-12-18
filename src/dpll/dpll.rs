3020d088adeb96fef2181ff34abaab885173ffd8
Maximilian Hofstetter <maximilian.hofstetter@gmx.net>
2025-12-18 12:11:35 +0100

    WIP

--- src/dpll/dpll.rs
+++ src/dpll/dpll.rs
@@ -44,37 +44,32 @@ impl Dpll {
         let branch_a = FromShortestClause::chose_next_assignment(&self.cnf_formula);
         let branch_b = branch_a.inverse();
 
-        if let Result::Satisfied = self.handle_assign_single_branch(branch_a) {
-            return Result::Satisfied;
-        }
 
-        if let Result::Satisfied = self.handle_assign_single_branch(branch_b) {
-            return Result::Satisfied;
+        for branch in [branch_a, branch_b] {
+
+            let assignment_result = self
+                .cnf_formula
+                .apply_assignment(&branch, &mut self.unit_queue);
+
+            if let AssignmentResult::Conflict = assignment_result {
+                self.cnf_formula.reverse_assignment(&branch);
+                self.unit_queue.clear();
+                continue;
+            }
+
+            self.assignment_stack.push_assignment(branch);
+            let branch_result = self.dpll();
+
+            match branch_result {
+                Result::Conflict => {
+                    self.assignment_stack.revert_last_assignment(&mut self.cnf_formula);
+                    self.unit_queue.clear();
+                }
+                Result::Satisfied => return Result::Satisfied
+            }
         }
 
         self.assignment_stack.revert_assignment_current_decision_level(&mut self.cnf_formula);
         Result::Conflict
-    }
-
-    fn handle_assign_single_branch(&mut self, assignment: Assignment) -> Result {
-        let assignment_result = self
-            .cnf_formula
-            .apply_assignment(&assignment, &mut self.unit_queue);
-
-        if let AssignmentResult::Conflict = assignment_result {
-            self.cnf_formula.reverse_assignment(&assignment);
-            self.unit_queue.clear();
-            return Result::Conflict
-        }
-
-        self.assignment_stack.push_assignment(assignment);
-        let branch_result = self.dpll();
-
-        if let Result::Conflict = branch_result {
-            self.assignment_stack.revert_last_assignment(&mut self.cnf_formula);
-            self.unit_queue.clear();
-        }
-
-        branch_result
     }
 }
