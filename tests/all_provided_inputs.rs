9463c9d4d30835b1fc310e991f98aec8f121df56
Maximilian Hofstetter <maximilian.hofstetter@gmx.net>
2026-01-02 18:59:40 +0100

    Add nextest with timeout
    # Conflicts:
    #	Cargo.toml

---
+++ tests/all_provided_inputs.rs
@@ -0,0 +1,33 @@
+use dpml::dpll::dpll::Dpll;
+use dpml::dpll::dpll::DpllResult::{Conflict, Satisfied};
+use dpml::parser::parse_cnf;
+use dpml::verify::verify_satisfied;
+use std::path::Path;
+use dpml::dpll::heuristics::from_shortest_clause::FromShortestClause;
+
+fn test_satisfied(_: &Path, input: String) -> datatest_stable::Result<()> {
+    let cnf = parse_cnf(input.as_str()).unwrap();
+    let heuristic = FromShortestClause;
+    let mut dpll = Dpll::new(cnf, heuristic);
+
+    let result = dpll.solve();
+    assert_eq!(result, Satisfied);
+    Ok(())
+}
+
+fn test_unsatisfiable(_: &Path, input: String) -> datatest_stable::Result<()> {
+    let cnf = parse_cnf(input.as_str()).unwrap();
+    let heuristic = FromShortestClause;
+    let mut dpll = Dpll::new(cnf, heuristic);
+
+    let result = dpll.solve();
+    assert_eq!(result, Conflict);
+    Ok(())
+}
+
+datatest_stable::harness! {
+    { test = test_satisfied, root = "./inputs/test/sat", pattern = r"^.*\.cnf$" },
+    { test = test_satisfied, root = "./inputs/sat", pattern = r"^.*\.cnf$" },
+    { test = test_unsatisfiable, root = "./inputs/test/unsat", pattern = r"^.*\.cnf$" },
+    { test = test_unsatisfiable, root = "./inputs/unsat", pattern = r"^.*\.cnf$" },
+}
