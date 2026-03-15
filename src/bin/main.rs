84e04c1f1e4b6c993896d985f483d72edd2f6392
Konstantin Grabmann <accounts.codeberg@kstn.in>
2026-03-15 14:53:55 +0100

    main

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,7 +2,10 @@ use clap::Parser;
 use dpml::cdcl::cdcl::{Cdcl, CdclStatus};
 use dpml::cli::CliArguments;
 use dpml::cnf::cnf_formula::CnfFormula;
-use dpml::parser::parse_cnf;
+use dpml::parser::{parse, parse_cnf};
+use dpml::preprocess::cnf::cnf::CNF;
+use dpml::preprocess::niver::recover_assigment_niver_compat;
+use dpml::preprocess::preprocess::preprocess;
 use std::fs;
 use std::time::Instant;
 
@@ -20,7 +23,7 @@ fn main() {
         }
     };
 
-    let cnf_formula: CnfFormula = match parse_cnf(&cnf_formula_str) {
+    let cnf_formula_pre = match parse(&cnf_formula_str) {
         Ok(value) => value,
         Err(_) => {
             println!("Failed to parse input file.");
@@ -28,27 +31,46 @@ fn main() {
         }
     };
 
+    let mut cnf = CNF::from_pre(&cnf_formula_pre.0, cnf_formula_pre.1);
     let heuristic = cli.load_heuristic();
 
-    let mut dpll = Cdcl::new(cnf_formula, heuristic);
+    let mut dpll = Cdcl::new(cnf.to_cnf_formula(), heuristic);
+
+    let start_pre = Instant::now();
+    let niver_trace = preprocess(&mut cnf);
+    let elapsed_pre = start_pre.elapsed();
 
     let start = Instant::now();
     let result = dpll.solve();
     let elapsed = start.elapsed();
 
+    let assignment = recover_assigment_niver_compat(niver_trace, cnf, dpll.cnf_formula);
+
     match result {
         CdclStatus::Sat => println!(
             "\
-s SATISFIABLE
-v {} 0
-t {:.7}",
-            dpll.cnf_formula.get_assignment_view(),
+s  SATISFIABLE
+v  {} 0
+tp {:.7}
+t  {:.7}",
+            assignment
+                .iter()
+                .map(|(key, &val)| if val {
+                    key.to_string()
+                } else {
+                    format!("-{}", key)
+                })
+                .collect::<Vec<String>>()
+                .join(" "),
+            elapsed_pre.as_secs_f64(),
             elapsed.as_secs_f64()
         ),
         CdclStatus::Unsat => println!(
             "\
-s UNSATISFIABLE
-t {:.7}",
+s  UNSATISFIABLE
+tp {:.7}
+t  {:.7}",
+            elapsed_pre.as_secs_f64(),
             elapsed.as_secs_f64()
         ),
         CdclStatus::Incomplete | CdclStatus::Conflict(_) => unreachable!(),
