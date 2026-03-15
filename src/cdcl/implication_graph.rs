b803f572ae3c053d3501d5a8a1fddcfeb43b7894
Maximilian Hofstetter <maximilian.hofstetter@gmx.net>
2026-03-15 09:23:00 +0100

    Add some debugging and display functionality

--- src/cdcl/implication_graph.rs
+++ src/cdcl/implication_graph.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+use std::fmt::Formatter;
 use crate::cdcl::assignment::Assignment;
 use crate::cnf::cnf_formula::CnfFormula;
 use crate::cnf::literals::Literals;
@@ -92,5 +94,23 @@ impl ImplicationGraph {
         self.trail.truncate(split_point);
         self.decision_level_start.truncate(desired_decision_level);
 
+    }
+}
+
+impl fmt::Display for ImplicationGraph {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            self.trail.iter()
+                .map(|assignment| {
+                    match assignment.reason {
+                        None => format!(" D:{}", assignment),
+                        Some(_) => format!("→{}", assignment),
+                    }
+
+                })
+                .collect::<Vec<String>>().join("")
+        )
     }
 }
