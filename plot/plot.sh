ef38e20c6d4a7c202c997d1ab9b00e35b6efe5be
Konstantin Grabmann <accounts.codeberg@kstn.in>
2026-03-15 23:39:59 +0100

    cleanup

--- plot/plot.sh
+++ plot/plot.sh
@@ -7,18 +7,14 @@ echo "done!"
 BIN=.././target/release/main
 
 generate_data() {
-  rm data/"$1"
+  rm -f data/"$1"
 
   find .././inputs -type f | while IFS= read -r f; do
     filename=$(basename "$f")
 
     echo "$filename"...
 
-    if [ -z "$2" ]; then
-      output=$(timeout 60s $BIN "$f" | grep "^t")
-    else
-      output=$(timeout 60s $BIN "--disable-preprocess" "$f" | grep "^t")
-    fi
+    output=$(timeout 60s $BIN "${@:2}" "$f" | grep "^t")
 
     if [ $? -eq 0 ]; then
       t=$(echo "$output" | grep "^t " | awk '{print $2}')
@@ -38,5 +34,21 @@ generate_data() {
   rm tmp.csv
 }
 
-generate_data "pre.csv"
-generate_data "nopre.csv" 0
+generate_data "luby.csv"
+generate_data "never.csv" "--restart-heuristic" "never"
+generate_data "fi.csv" "--restart-heuristic" "fixed-interval"
+generate_data "geo.csv" "--restart-heuristic" "geometric"
+generate_data "ps_luby.csv" "--phase-saving"
+generate_data "ps_never.csv" "--restart-heuristic" "never" "--phase-saving"
+generate_data "ps_fi.csv" "--restart-heuristic" "fixed-interval" "--phase-saving"
+generate_data "ps_geo.csv" "--restart-heuristic" "geometric" "--phase-saving"
+generate_data "ps_nopre.csv" "--disable-preprocess"
+generate_data "np_luby.csv" "--disable-preprocess"
+generate_data "np_never.csv" "--restart-heuristic" "never" "--disable-preprocess"
+generate_data "np_fi.csv" "--restart-heuristic" "fixed-interval" "--disable-preprocess"
+generate_data "np_geo.csv" "--restart-heuristic" "geometric" "--disable-preprocess"
+generate_data "np_ps_luby.csv" "--phase-saving" "--disable-preprocess"
+generate_data "np_ps_never.csv" "--restart-heuristic" "never" "--phase-saving" "--disable-preprocess"
+generate_data "np_ps_fi.csv" "--restart-heuristic" "fixed-interval" "--phase-saving" "--disable-preprocess"
+generate_data "np_ps_geo.csv" "--restart-heuristic" "geometric" "--phase-saving" "--disable-preprocess"
+generate_data "np_ps_nopre.csv" "--disable-preprocess" "--disable-preprocess"
